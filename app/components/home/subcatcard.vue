c101abaad389a19d7cf0d8f206168885ce4bc1cd
devnydv <bittumailb2@gmail.com>
2026-04-08 18:13:20 +0530

    feat: enhance subcategory card component with dynamic data binding and improved structure

--- app/components/home/subcatcard.vue
+++ app/components/home/subcatcard.vue
@@ -2,24 +2,24 @@
     
 
 
+
       
 
-       
-
-        <NuxtLink to="/ncert/books" class="card">
+        <NuxtLink :to="`/${item.category}/${item.subcat.slug}/${item.slug}/`" class="card">
           <div class="card-img" style="background:linear-gradient(135deg,#1a1000,#3d2800,#6b4500);">
-            <div class="card-img-text">🧠</div>
+            <div class="card-img-text">{{item.emoji}}</div>
           </div>
           <div class="card-body">
-            <div class="card-title">General Knowledge Starter</div>
-            <div class="card-desc">A relaxed mix of everyday facts — the perfect first quiz if you just want to dip your toes in.</div>
-            <div class="card-meta">
-              <span>📋 8 Chapters</span>
-              <span>⏱ 6 MCQs</span>
+            <div class="card-title">{{item.names}}</div>
+            <div class="card-desc">{{item.desc}}</div>
+            <div class="card-meta" >
+              <span>📋 {{ item.count[0] }} Chapters</span>
+              
+              <span>⏱ {{ item.count[1] }} MCQs</span> 
             </div>
             <div class="card-chips">
-              <span class="card-chip" style="color:#3df0c8;border-color:rgba(61,240,200,0.3);background:rgba(61,240,200,0.07);">🌱 Beginner</span>
-              <span class="card-chip" style="color:#3df0c8;border-color:rgba(61,240,200,0.3);background:rgba(61,240,200,0.07);">General</span>
+              <!-- <span class="card-chip" style="color:#3df0c8;border-color:rgba(61,240,200,0.3);background:rgba(61,240,200,0.07);">🌱 Beginner</span> -->
+              <span class="card-chip" style="color:#3df0c8;border-color:rgba(61,240,200,0.3);background:rgba(61,240,200,0.07);">{{item.category}}</span>
             </div>
             <!-- <div class="card-actions">
               <button class="card-btn-learn">📖 Quick Guide</button>
@@ -36,6 +36,9 @@
     
 </template>
 <script setup>
-const {data}  = defineProps(['data'])
+const {item}  = defineProps({item: {
+        type: Object,
+        required: true
+    }})
 
 </script>
