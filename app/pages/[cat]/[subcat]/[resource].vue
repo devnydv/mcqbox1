36c2de6b1e981eef94ee1ac64ecc8a50f7069eb8
devnydv <bittumailb2@gmail.com>
2026-04-12 10:58:42 +0530

    feat: update topic hero section to dynamically display subcategory and topic details

--- app/pages/[cat]/[subcat]/[resource].vue
+++ app/pages/[cat]/[subcat]/[resource].vue
@@ -1,10 +1,11 @@
 <template>
      <section class="topic-hero">
+        
         <div class="topic-hero-inner" v-for="(items, index) in content" :key="index">
-            <div class="topic-tag fade-up"><span class="topic-tag-dot"></span> sdfgdfg</div>
+            <div class="topic-tag fade-up"><span class="topic-tag-dot"></span> {{items[0].subcat}}</div>
             <!-- <h1 class="fade-up fade-up-1"> <em>Books</em></h1> -->
-            <h1 class="fade-up fade-up-1">sdfgsdfg</h1>
-            <p class="topic-hero-desc fade-up fade-up-2">sfgsdfg</p>
+            <h1 class="fade-up fade-up-1">{{items[0].topic.names}}</h1>
+            <p class="topic-hero-desc fade-up fade-up-2">{{items[0].topic.desc}}</p>
         </div>
         
     </section> 
